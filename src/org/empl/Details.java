7b34995af74c8812633c8db9be819a39102f87b2
Benishia <benishiamb23@gmail.com>
2021-10-07 16:08:21 +0530

    Folder Structure

---
+++ src/org/empl/Details.java
@@ -0,0 +1,43 @@
+package org.empl;
+
+import java.util.Scanner;
+
+public class Details {
+	
+
+	public static void main(String[] args) {
+		
+		Scanner s= new Scanner(System.in);
+		
+		System.out.println("Enter Employee Name:");
+		String empName=s.nextLine();
+		System.out.println("Employee Name:"+empName);
+		
+		System.out.println("Enter employee id:");
+		byte empId=s.nextByte();
+		System.out.println("Employee Id:"+empId);
+		
+		System.out.println("Enter Employee EmailId:");
+		String empEmail=s.next();
+		System.out.println("Employee EmailId:"+empEmail);
+		
+		System.out.println("Enter Employee Phone number");
+	    long empPhoneno=s.nextLong();
+		System.out.println("Employee Phone number:"+empPhoneno);
+		
+		System.out.println("Enter Employee Salary:");
+		double empSalary=s.nextDouble();
+		System.out.println("Employee Salary:"+empSalary);
+		
+		System.out.println("Enter Employee Gender:");
+		String empGender=s.next();
+		System.out.println("Employee Gender:"+empGender);
+		
+		System.out.println("Enter Employee City:");
+		String empCity=s.nextLine();
+		System.out.println("Employee City:"+empCity);
+		
+		s.close();
+	}
+
+}
